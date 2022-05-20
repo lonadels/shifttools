dda5b30d84ad1bbde41adb2a105c66cc396792d3
lonaddels <lonadels@ya.ru>
2022-05-20 13:02:32 +0300

    Time pickers;

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,37 @@
-import {Box, Container} from "@mui/material";
-import Link from "../components/Link";
-import LordIcon from "../components/LordIcon";
-import Button from "@mui/material/Button";
+import {Box, Container, TextField} from "@mui/material";
+import {LocalizationProvider, TimePicker} from "@mui/x-date-pickers";
+// date-fns
+import {AdapterDateFns} from '@mui/x-date-pickers/AdapterDateFns';
+import {useState} from "react";
+
+import ruLocale from 'date-fns/locale/ru';
+
 
 export default function Home() {
+    const [value, setValue] = useState(new Date());
+
+    const handleChange = (newValue) => {
+        setValue(newValue);
+    };
 
     return (
         <div>
             <Container maxWidth="md">
                 <Box>
-                    <Button startIcon={<LordIcon state="hover-1"
-                                                 src="syzergpm"
-                                                 trigger="loop-on-hover"
-                                                 target="#about"/>}
-                            color={"primary"} id="about"
-                            variant="contained"
-                            component={Link}
-                            size={"large"}
-                            noLinkStyle
-                            href="/about">
-                        Информация
-                    </Button>
+                    <LocalizationProvider locale={ruLocale} dateAdapter={AdapterDateFns}>
+                        <TimePicker
+                            label="Начало смены"
+                            value={value}
+                            onChange={handleChange}
+                            renderInput={(params) => <TextField {...params} />}
+                        />
+                        <TimePicker
+                            label="Конец смены"
+                            value={value}
+                            onChange={handleChange}
+                            renderInput={(params) => <TextField {...params} />}
+                        />
+                    </LocalizationProvider>
                 </Box>
             </Container>
         </div>
