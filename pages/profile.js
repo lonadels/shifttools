d742be171188cd593363efaa29a2978e3dfeb76b
lonaddels <lonadels@ya.ru>
2022-05-23 02:24:56 +0300

    Items;

--- pages/profile.js
+++ pages/profile.js
@@ -2,41 +2,69 @@ import * as React from 'react';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import {ToggleButton, ToggleButtonGroup} from "@mui/lab";
-import {AutoFixHighRounded, DarkModeRounded, LightModeRounded, PaletteRounded} from "@mui/icons-material";
 import {
-    Avatar,
+    AnimationRounded,
+    AutoFixHighRounded, BugReportRounded,
+    DarkModeRounded, HeadsetMicRounded, HelpOutlineRounded,
+    InfoRounded,
+    LightModeRounded,
+    ManageAccountsRounded,
+    OpenInNewRounded,
+    PaletteRounded, QuestionMarkRounded
+} from "@mui/icons-material";
+import {
+    Avatar, Divider,
     List,
     ListItem,
+    ListItemButton,
     ListItemIcon,
     ListItemText,
     ListSubheader,
     Paper,
     Stack,
+    Switch,
     Typography
 } from "@mui/material";
-import useMediaQuery from "@mui/material/useMediaQuery";
+import {useBooleanState} from "webrix/hooks";
 
+import * as packageInfo from '../package.json';
+
+const version = packageInfo.version;
 
 export default function Profile() {
     const handleChange = (event, newAlignment) => {
         //setAlignment(newAlignment);
     };
 
-    return (
-        <Container maxWidth={'md'}>
-            <Box sx={{py: 2}}>
-                <Paper variant={'outlined'}>
-                    <Container sx={{py: 2}}>
-                        <Stack direction="row"
-                               alignItems="center"
-                               spacing={2}>
-                            <Avatar />
-                            <Typography variant="h6" gutterBottom>Владислав Шинкарук</Typography>
-                        </Stack>
-                    </Container>
-                </Paper>
-                <List  subheader={<ul />}>
-                    <ListItem disableGutters>
+    const checked = useBooleanState(false);
+    return (<Box sx={{py: 2}}>
+            <Container sx={{p: 0}} maxWidth={'md'}>
+                <Container sx={{p: {
+                    sm: 2
+                    }}}>
+                    <Paper variant={"outlined"}>
+                        <Container sx={{py: 2}}>
+                            <Stack direction="row"
+                                   alignItems="center"
+                                   spacing={{xs: 2, sm: 3}}>
+                                <Avatar sx={{width: 48, height: 48}}/>
+                                <Stack direction={"column"} spacing={0}>
+                                    <Typography variant="h6" component="div">Владислав Шинкарук</Typography>
+                                    <Typography variant="body2" color={"text.secondary"} component="div">Junior Shift
+                                        Manager</Typography>
+                                </Stack>
+                            </Stack>
+                        </Container>
+                    </Paper>
+                </Container>
+                {/*<TextField
+                    sx={{mt: 2}}
+                    placeholder={"Поиск"}
+                    fullWidth
+                    InputProps={{startAdornment: <InputAdornment position={"start"}><SearchRounded/></InputAdornment>}}
+                ></TextField>*/}
+                <List sx={{mt: 2}} subheader={<ListSubheader>Внешний вид</ListSubheader>}>
+                    <ListItem>
                         <ListItemIcon>
                             <PaletteRounded/>
                         </ListItemIcon>
@@ -54,8 +82,56 @@ export default function Profile() {
                             <ToggleButton value="system"><AutoFixHighRounded/></ToggleButton>
                         </ToggleButtonGroup>
                     </ListItem>
+                    <ListItemButton disabled>
+                        <ListItemIcon>
+                            <AnimationRounded/>
+                        </ListItemIcon>
+                        <ListItemText id="switch-list-label-wifi" primary="Анимации"
+                                      secondary={"Воспроизведение анимаций может повлиять на производительность"}/>
+                        <Switch
+                            edge="end"
+                            checked={checked}
+                            inputProps={{
+                                'aria-labelledby': 'switch-list-label-bluetooth',
+                            }}
+                        />
+                    </ListItemButton>
                 </List>
-            </Box>
-        </Container>
-    );
+                <List subheader={<ListSubheader>О приложении</ListSubheader>}>
+                    <ListItem>
+                        <ListItemIcon>
+                            <InfoRounded/>
+                        </ListItemIcon>
+                        <ListItemText id="switch-list-label-wifi" primary="Версия"
+                                      secondary={version}/>
+                    </ListItem>
+                    <ListItemButton>
+                        <ListItemIcon>
+                            <ManageAccountsRounded/>
+                        </ListItemIcon>
+                        <ListItemText id="switch-list-label-wifi" primary="Разработчик"
+                                      secondary="SwiftSoft"/>
+                        <OpenInNewRounded/>
+                    </ListItemButton>
+                </List>
+                <List subheader={<ListSubheader>Помощь</ListSubheader>}>
+                    <ListItemButton>
+                        <ListItemIcon>
+                            <HeadsetMicRounded/>
+                        </ListItemIcon>
+                        <ListItemText id="switch-list-label-wifi" primary="Поддержка"/>
+                        <OpenInNewRounded/>
+                    </ListItemButton>
+                    <ListItemButton>
+                        <ListItemIcon>
+                            <BugReportRounded/>
+                        </ListItemIcon>
+                        <ListItemText id="switch-list-label-wifi" primary="Сообщить об ошибке"/>
+                        <OpenInNewRounded/>
+                    </ListItemButton>
+                </List>
+            </Container>
+        </Box>
+    )
+        ;
 }
--- pages/tools/index.js
+++ pages/tools/index.js
@@ -1,69 +1,138 @@
 import * as React from 'react';
-import {List, ListItemButton, ListItemIcon, ListItemText, ListSubheader} from "@mui/material";
+import {Divider, List, ListItemButton, ListItemIcon, ListItemText, ListSubheader} from "@mui/material";
 import {useRouter} from "next/router";
 import {
     AutoGraphRounded,
+    BadgeRounded,
     FastfoodRounded,
+    HelpOutlineRounded,
+    LocalDrinkRounded,
+    LocalOfferRounded,
+    LocalShippingRounded,
     NavigateNextRounded,
+    OpenInNewRounded,
     ScheduleRounded,
+    ScienceRounded,
     WorkspacePremiumRounded
 } from "@mui/icons-material";
 
 export default function Tools() {
     const router = useRouter();
     return (
-        <List sx={{
-            width: '100%',
-            height: '100%',
+        <>
+            <List sx={{
+                '& ul': {padding: 0},
+            }}
+                  subheader={<li/>}>
+                <li>
+                    <ul>
+                        <ListSubheader>{`Считалки`}</ListSubheader>
 
-            '& ul': {padding: 0},
-        }}
-              subheader={<li/>}>
-            <li key={0}>
-                <ul>
-                    <ListSubheader>{`Считалки`}</ListSubheader>
-                    <ListItemButton disabled>
-                        <ListItemIcon>
-                            <FastfoodRounded/>
-                        </ListItemIcon>
-                        <ListItemText primary="Расчет поставки"
-                                      secondary={"Расчет необходимого количества продукта для заказа (скоро)"}/>
-                        <NavigateNextRounded/>
-                    </ListItemButton>
-                    <ListItemButton onClick={() => {
-                        // TODO: make component
-                        router.push(`${router.pathname}/hours`, undefined, {shallow: true});
-                    }}>
-                        <ListItemIcon>
-                            <ScheduleRounded/>
-                        </ListItemIcon>
-                        <ListItemText primary="Часы сотрудников"
-                                      secondary={"Подсчет рабочего времени персонала"}/>
-                        <NavigateNextRounded/>
-                    </ListItemButton>
-                </ul>
-            </li>
-            <li key={1}>
-                <ul>
-                    <ListSubheader>{`Другое`}</ListSubheader>
-                    <ListItemButton disabled>
-                        <ListItemIcon>
-                            <AutoGraphRounded/>
-                        </ListItemIcon>
-                        <ListItemText primary="Реализация"
-                                      secondary={"Таблица отчетности показателей ресторана (скоро)"}/>
-                        <NavigateNextRounded/>
-                    </ListItemButton>
-                    <ListItemButton disabled>
-                        <ListItemIcon>
-                            <WorkspacePremiumRounded/>
-                        </ListItemIcon>
-                        <ListItemText primary="Сертификация"
-                                      secondary={"Чек-лист сертификации сотрудника на позицию (скоро)"}/>
-                        <NavigateNextRounded/>
-                    </ListItemButton>
-                </ul>
-            </li>
-        </List>
+                        <ListItemButton onClick={() => {
+                            // TODO: make component
+                            router.push(`${router.pathname}/hours`, undefined, {shallow: true});
+                        }}>
+                            <ListItemIcon>
+                                <ScheduleRounded/>
+                            </ListItemIcon>
+                            <ListItemText primary="Часы сотрудников"
+                                          secondary={"Подсчет рабочего времени персонала"}/>
+                            <NavigateNextRounded/>
+                        </ListItemButton>
+                        <ListItemButton disabled>
+                            <ListItemIcon>
+                                <FastfoodRounded/>
+                            </ListItemIcon>
+                            <ListItemText primary="Расчет поставки"
+                                          secondary={"Расчет необходимого для заказа количества охлажденного куриного п/ф"}/>
+                            <NavigateNextRounded/>
+                        </ListItemButton>
+                    </ul>
+                </li>
+                <li>
+                    <ul>
+                        <ListSubheader>{`Чек-листы`}</ListSubheader>
+                        <ListItemButton disabled>
+                            <ListItemIcon>
+                                <ScienceRounded/>
+                            </ListItemIcon>
+                            <ListItemText primary={`ООО "ТД Полимир"`}
+                                          secondary={"Бланк подсчета химии и упаковки"}/>
+                            <NavigateNextRounded/>
+                        </ListItemButton>
+                        <ListItemButton disabled>
+                            <ListItemIcon>
+                                <LocalShippingRounded/>
+                            </ListItemIcon>
+                            <ListItemText primary={`ООО "РБД-Дистрибьюция"`}
+                                          secondary={"Бланк подсчета замороженной и охлажденной продукции"}/>
+                            <NavigateNextRounded/>
+                        </ListItemButton>
+                        <ListItemButton disabled>
+                            <ListItemIcon>
+                                <LocalDrinkRounded/>
+                            </ListItemIcon>
+                            <ListItemText primary={`ООО "ПепсиКо Холдингс"`}
+                                          secondary={"Бланк подсчета бибов, бутылок и баллонов Pepsi"}/>
+                            <NavigateNextRounded/>
+                        </ListItemButton>
+                       {/* <Divider variant={"middle"}/>*/}
+                        <ListItemButton disabled>
+                            <ListItemIcon>
+                                <WorkspacePremiumRounded/>
+                            </ListItemIcon>
+                            <ListItemText primary="Сертификация"
+                                          secondary={"Бланк сертификации сотрудника на позицию (скоро)"}/>
+                            <NavigateNextRounded/>
+                        </ListItemButton>
+                    </ul>
+                </li>
+                <li>
+                    <ul>
+                        <ListSubheader>{`Отчеты`}</ListSubheader>
+                        <ListItemButton disabled>
+                            <ListItemIcon>
+                                <AutoGraphRounded/>
+                            </ListItemIcon>
+                            <ListItemText primary="Реализация"
+                                          secondary={"Таблица отчетности показателей ресторана (скоро)"}/>
+                            <NavigateNextRounded/>
+                        </ListItemButton>
+                    </ul>
+                </li>
+                <li>
+                    <ul>
+                        <ListSubheader>{`Другое`}</ListSubheader>
+                        <ListItemButton disabled>
+                            <ListItemIcon>
+                                <BadgeRounded/>
+                            </ListItemIcon>
+                            <ListItemText primary="Поставщики"
+                                          secondary={"Справочная информация по поставщикам"}/>
+                            <NavigateNextRounded/>
+                        </ListItemButton>
+                        <ListItemButton disabled>
+                            <ListItemIcon>
+                                <LocalOfferRounded/>
+                            </ListItemIcon>
+                            <ListItemText primary="Производители"
+                                          secondary={"Названия производителей сырья"}/>
+                            <NavigateNextRounded/>
+                        </ListItemButton>
+                    </ul>
+                </li>
+            </List>
+            <Divider/>
+            <List subheader={<li/>}>
+                <ListItemButton>
+                    <ListItemIcon>
+                        <HelpOutlineRounded/>
+                    </ListItemIcon>
+                    <ListItemText id="switch-list-label-wifi" primary="Не хватает нужного инструмета?"
+                                  secondary="Напишите нам об этом"/>
+                    <OpenInNewRounded/>
+                </ListItemButton>
+            </List>
+        </>
     );
 }
